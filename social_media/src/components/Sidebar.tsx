3b19ff2bc3557d7e755c21bd986cb41220d5a29d
Sohaila zidan <sohailaziedan533@gmail.com>
2025-08-17 18:51:26 +0300

    update links

--- social_media/src/components/Sidebar.tsx
+++ social_media/src/components/Sidebar.tsx
@@ -8,10 +8,10 @@ import {
   faListUl,
   faUser,
   faEllipsis,
-  faRightFromBracket
-  
-  
+  faRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+
 import { useNavigate } from "react-router-dom";
 import { authService } from "../services/authService";
 
@@ -24,52 +24,64 @@ const Sidebar = () => {
   };
 
   return (
-    <div >
+    <div>
+      <nav className="mt-5">
+        <NavLink
+        to="/home"
+        className={({ isActive }) =>
+          `mb-8  ${
+            isActive ? "link-active" : "link"
+          }`
+        }
+      >
+        <FontAwesomeIcon icon={faHouse} className="text-xl" />
+        <span className="icon">Home</span>
+      </NavLink>
 
-  <nav className="mt-5">
-    <div className="link-active mb-8">
-      <FontAwesomeIcon icon={faHouse} className="text-xl" />
-      <span className="icon">Home</span>
-    </div>
-    <div className="link mb-8">
-      <FontAwesomeIcon icon={faHashtag} className="text-xl" />
-      <span className="icon">Explore</span>
-    </div>
-    <div className="link mb-8">
-      <FontAwesomeIcon icon={faBell} className="text-xl" />
-      <span className="icon">Notifications</span>
-    </div>
-    <div className="link mb-8">
-      <FontAwesomeIcon icon={faEnvelope} className="text-xl" />
-      <span className="icon">Messages</span>
-    </div>
-    <div className="link mb-8">
-      <FontAwesomeIcon icon={faBookmark} className="text-xl" />
-      <span className="icon">Bookmarks</span>
-    </div>
-    <div className="link mb-8">
-      <FontAwesomeIcon icon={faListUl} className="text-xl" />
-      <span className="icon">Lists</span>
-    </div>
-    <div className="link mb-8">
-      <FontAwesomeIcon icon={faUser} className="text-xl" />
-      <span className="icon">Profile</span>
-    </div>
-    <div className="link mb-8">
-      <FontAwesomeIcon icon={faEllipsis} className="text-xl" />
-      <span className="icon">More</span>
-    </div>
-      <div
-          onClick={handleLogout}
-          className="link mb-8 cursor-pointer "
-        >
+      <NavLink
+        to="/profile"
+        className={({ isActive }) =>
+          `mb-8  ${
+            isActive ? "link-active" : "link"
+          }`
+        }
+      >
+        <FontAwesomeIcon icon={faUser} className="text-xl" />
+        <span className="icon">Profile</span>
+      </NavLink>
+ 
+        <div className="link mb-8">
+          <FontAwesomeIcon icon={faHashtag} className="text-xl" />
+          <span className="icon">Explore</span>
+        </div>
+        <div className="link mb-8">
+          <FontAwesomeIcon icon={faBell} className="text-xl" />
+          <span className="icon">Notifications</span>
+        </div>
+        <div className="link mb-8">
+          <FontAwesomeIcon icon={faEnvelope} className="text-xl" />
+          <span className="icon">Messages</span>
+        </div>
+        <div className="link mb-8">
+          <FontAwesomeIcon icon={faBookmark} className="text-xl" />
+          <span className="icon">Bookmarks</span>
+        </div>
+        <div className="link mb-8">
+          <FontAwesomeIcon icon={faListUl} className="text-xl" />
+          <span className="icon">Lists</span>
+        </div>
+        
+        <div className="link mb-8">
+          <FontAwesomeIcon icon={faEllipsis} className="text-xl" />
+          <span className="icon">More</span>
+        </div>
+        <div onClick={handleLogout} className="link mb-8 cursor-pointer ">
           <FontAwesomeIcon icon={faRightFromBracket} className="text-xl" />
           <span className="icon">Logout</span>
         </div>
-  </nav>
-</div>
+      </nav>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Sidebar
+export default Sidebar;
